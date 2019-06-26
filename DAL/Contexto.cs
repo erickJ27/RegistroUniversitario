3c4c56a8cf5b2d415ae16531a80b97ccf1f8893b
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-06-25 21:44:29 -0400

    Contexto realizado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using RegistroUniversitario.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroUniversitario.DAL
+{
+    public class Contexto:DbContext
+    {
+        public DbSet<Asignaturas> Asignaturas { get; set; }
+        public DbSet<Estudiantes> Estudiantes { get; set; }
+        public DbSet<Inscripciones> Inscripciones { get; set; }
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
