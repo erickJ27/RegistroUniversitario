59c99876917c86452c0f5fc8e0530d0dc6889258
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-06-25 20:31:19 -0400

    Entidad Estudiante creada

---
+++ Entidades/Estudiantes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroUniversitario.Entidades
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Nombre { get; set; }
+        public decimal Balance { get; set; }
+
+        public Estudiantes()
+        {
+            EstudianteId = 0;
+            FechaIngreso = DateTime.Now;
+            Nombre = string.Empty;
+            Balance = 0;
+        }
+
+
+    }
+}
