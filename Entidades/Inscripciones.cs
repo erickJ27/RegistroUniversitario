8ff63e74209c19796f35f1089909f28e9daa7003
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2019-06-25 21:24:16 -0400

    Se creo las Entidades InscripcionDetalle y Inscripciones

---
+++ Entidades/Inscripciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroUniversitario.Entidades
+{
+    public class Inscripciones
+    {
+        [Key]
+        public int InscripcionId { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public decimal Monto { get; set; }
+
+        public virtual List<InscripcionesDetalle> Detalle { get; set; }
+        public Inscripciones()
+        {
+            InscripcionId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+        }
+
+    }
+}
---
+++ Entidades/InscripcionesDetalle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroUniversitario.Entidades
+{
+    public class InscripcionesDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int AsignaturaId { get; set; }
+
+        public int EstudianteId { get; set; }
+
+        //Todo:Falta todavia algun otro campo
+
+        public InscripcionesDetalle()
+        {
+            Id = 0;
+            AsignaturaId = 0;
+            EstudianteId = 0;
+        }
+
+        public InscripcionesDetalle(int Id,int AsignaturaId,int EstudianteId)
+        {
+            this.Id = Id;
+            this.AsignaturaId =AsignaturaId;
+            this.EstudianteId =EstudianteId;
+        }
+    }
+}
